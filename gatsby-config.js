26fe0a7a2babca5868c0ffada5d418cb56599f48
Jayce hwang <jayce.hwang.dev@gmail.com>
2020-09-17 23:51:49 +0900

    Add package about data layer

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
     pathPrefix: "/blog",
 
 
-    plugins: [`gatsby-plugin-material-ui`],
+    plugins: [
+        `gatsby-plugin-material-ui`,
+        `gatsby-transformer-remark`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/contents/`,
+            },
+        },
+    ],
 }
